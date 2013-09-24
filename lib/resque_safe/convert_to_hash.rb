94cbbf88cb376bca3ba7f1f15adb3ab246fde8ec
Robert R. Meyer <Blue.Dog.Archolite@gmail.com>
2013-09-24 16:48:41 -0500

    Correction for variable names

--- lib/resque_safe/convert_to_hash.rb
+++ lib/resque_safe/convert_to_hash.rb
@@ -6,19 +6,20 @@ module ResqueSafe
       args = args.flatten if args.respond_to?(:flatten)
 
       args.each do |arg|
-        next if a.blank?
-        if a.is_a?(Hash)
-          a.each do|k,v|
+        next if arg.blank?
+        arg_class = arg.class.name
+        if arg.is_a?(Hash)
+          arg.each do|k,v|
             if v.respond_to?(:gsub)
               th[k.to_sym] = CGI.escapeHTML(v)
             else
               th[k.to_sym] = v
             end
           end
-        elsif known_models.include?(a.class.name) || a.class.respond_to?(:column_names)
-          th[a.class.name.underscore.to_sym] = a.id
+        elsif known_models.include?(arg_class) || arg.class.respond_to?(:column_names)
+          th[arg_class.underscore.to_sym] = arg.id
         else
-          raise ArgumentError.new("Unknown #{a.class} passed to resque safe. \n Object #{a.inspect} \n\n Args #{args} \n\n Known Models #{known_models}")
+          raise ArgumentError.new("Unknown #{arg_class} passed to resque safe. \n Object #{arg.inspect} \n\n Args #{args} \n\n Known Models #{known_models}")
         end
       end
       th
